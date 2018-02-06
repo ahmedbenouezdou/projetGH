d97e85f9aeecbc31b92f7039645aca8c18ebfb8b
ahmedbenouezdou <ahmed.b.ouezdou@gmail.com>
2018-02-06 22:51:49 +0100

    update

--- src/service/routeService.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var app = express();
-var greetings = require("./activities/activitiesService");
-
-app.route('activitie').get(greetings.getActivities).post(greetings.addActivities);
